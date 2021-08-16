155cbad453f12356779ee1c9712c224fe0517c89
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2021-08-16 23:40:45 +0430

    Import libs

---
+++ RachelCore/RachelCore.py
@@ -0,0 +1,15 @@
+"""
+    RachelCore
+
+    Amirhossein Mohammadi
+
+    https://github.com/BlackIQ/Rachel
+    https://github.com/BlackIQ/RachelCore
+"""
+
+import os
+import webbrowser
+import googlesearch
+from time import *
+import random
+import requests
